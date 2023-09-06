b49cd3d11514df71f6663251cd23a3f174915d6b
Razie <rzyh1381@gmail.com>
2023-09-06 14:28:02 +0330

    add active states

---
+++ index.js
@@ -0,0 +1,31 @@
+$(".pages").on("mouseover", function () {
+    $(this).css("color", "hsl(5, 85%, 63%)");
+});
+
+$(".pages").on("mouseout", function () {
+    $(this).css("color", "hsl(236, 13%, 42%)");
+});
+
+$(".read-more").on("mouseover", function () {
+    $(this).addClass("hover");
+});
+
+$(".read-more").on("mouseout", function () {
+    $(this).removeClass("hover");
+});
+
+$(".buttom-box > .title").on("mouseover", function () {
+    $(this).css("color", "hsl(5, 85%, 63%)");
+});
+
+$(".buttom-box > .title").on("mouseout", function () {
+    $(this).css("color", "hsl(240, 100%, 5%)")
+});
+
+$(".new-box > .title").on("mouseover", function () {
+    $(this).css("color", "hsl(35, 77%, 62%)");
+});
+
+$(".new-box > .title").on("mouseout", function () {
+    $(this).css("color", "hsl(36, 100%, 99%)")
+});
